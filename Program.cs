207476dd80f5002f870b4b6d18e6bce60ec155b3
michu <michu@DESKTOP-LEHNONA>
2023-07-07 19:07:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using OOP_deber;
+
+try
+{
+    var fecha1 = new Date()
+    {
+        Year = 2023,
+        Month = 2,
+        Day = 28
+    };
+    Console.WriteLine(fecha1);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
